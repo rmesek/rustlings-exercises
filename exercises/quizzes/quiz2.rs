ba2832e2db05a3f3810db4fd8cd42aff12ac7431
Robert Mesek <77611276+rmesek@users.noreply.github.com>
2026-08-04 18:25:05 +0200

    Complete quiz2

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -28,6 +28,21 @@ mod my_module {
 
     // TODO: Complete the function as described above.
     // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output = Vec::new();
+
+        for (string, command) in input {
+            let new_string = match command {
+                Command::Uppercase => string.to_uppercase(),
+                Command::Trim => String::from(string.trim()),
+                Command::Append(n) => format!("{string}{}", "bar".repeat(n)),
+            };
+            
+            output.push(new_string);
+        }
+
+        output
+    }
 }
 
 fn main() {
@@ -39,6 +54,7 @@ mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
     // use ???;
     use super::Command;
+    use crate::my_module::transformer;
 
     #[test]
     fn it_works() {
