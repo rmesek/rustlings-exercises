b41379ffd0dad6b0d93b861d2687b4ad34cc0d2f
Robert Mesek <77611276+rmesek@users.noreply.github.com>
2026-08-24 21:39:02 +0200

    Complete 23_conversions

--- exercises/23_conversions/from_into.rs
+++ exercises/23_conversions/from_into.rs
@@ -34,7 +34,24 @@ impl Default for Person {
 // 5. Parse the second element from the split operation into a `u8` as the age.
 // 6. If parsing the age fails, return the default of `Person`.
 impl From<&str> for Person {
-    fn from(s: &str) -> Self {}
+    fn from(s: &str) -> Self {
+        let Some((name, age)) = s.split_once(',') else {
+            return Person::default();
+        };
+
+        if name.is_empty() {
+            return Person::default();
+        }
+
+        let Ok(age) = age.parse::<u8>() else {
+            return Person::default();
+        };
+
+        Person {
+            name: name.to_string(),
+            age,
+        }
+    }
 }
 
 fn main() {
